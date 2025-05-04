58ea3b59727304258027155676f943e278ff5d14
PedinFL <147783348+PedinFL@users.noreply.github.com>
2025-05-04 02:00:03 +0000

    CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
-// Jogo Super Trunfo.
-
-
 int main(){
 
 char cidade01[20], cidade02[20], estado01[2], estado02[2];
 int populacao01, populacao02, pontos01, pontos02;
 float area01, area02, pib01, pib02;
+//variaveis separadas para melhor visualização do trabalho
+float densidade01, densidade02, per_capita01, per_capita02;           //Atualização jogo super trunfo
+//float propriedades01, propriedades02, propriedades03, propriedades04; //Atualização jogo super trunfo
+
 
  // Primeira carta
  // Aqui será inserido os dados da primeira carta pelo usuário. 
 
     printf("Insira os dados da primeira carta!\n");
 
-    printf("Digite o estado: ");
+    printf("Digite o estado (A ou B): ");
     scanf(" %s", &estado01);
     
     printf("Digite a cidade: ");
@@ -32,6 +33,9 @@ float area01, area02, pib01, pib02;
     printf("Quantos pontos turisticos a cidade possui: ");
     scanf("%d", &pontos01);
 
+    densidade01 = populacao01 / area01; //Atualização jogo super trunfo
+    per_capita01 = pib01 / populacao01; //Atualização jogo super trunfo
+
     printf("\n"); // Foi utilizado para saltar linha, tornando o programa mais limpo.
 
  // Segunda carta
@@ -39,7 +43,7 @@ float area01, area02, pib01, pib02;
 
     printf("Insira os dados da segunda carta!\n");
 
-    printf("Digite o estado: ");
+    printf("Digite o estado (A ou B): ");
     scanf(" %s", &estado02);
 
     printf("Digite a cidade: ");
@@ -57,29 +61,36 @@ float area01, area02, pib01, pib02;
     printf("Quantos pontos turisticos a cidade possui: ");
     scanf("%d", &pontos02);
 
+    densidade02 = populacao02 / area02; //Atualização jogo super trunfo
+    per_capita02 = pib02 / populacao02;
+
     printf("\n"); // Foi utilizado para saltar linha, tornando o programa mais limpo.
 
  // Aqui é a área de impressão das duas cartas inseridas pelos usuários.
 
-    printf("Carta 1\n");
+    printf("*** Carta N°1 ***\n\n");
     printf("Estado: %s\n", estado01);
     printf("Código: %s01\n", estado01);
     printf("Nome da cidade: %s\n", cidade01);
     printf("População: %d\n", populacao01);
     printf("Área: %.2f Km²\n", area01);
-    printf("PIB: %.2f Bilhões de reais.\n", pib01);
-    printf("Número de Pontos Turísticos: %d\n\n", pontos01);
+    printf("PIB: %.2f Bilhões de reais\n", pib01);
+    printf("Número de Pontos Turísticos: %d\n", pontos01);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade01); //Atualização jogo super trunfo
+    printf("Pib Per capita: %.2f reais\n", per_capita01); //Atualização jogo super trunfo
 
     printf("\n"); // Foi utilizado para saltar linha, tornando o programa mais limpo.
 
-    printf("Carta N°2\n");
+    printf("*** Carta N°2 ***\n\n");
     printf("Estado: %s\n", estado02);
     printf("Código: %s02\n", estado02);
     printf("Nome da cidade: %s\n", cidade02);
     printf("População: %d\n", populacao02);
     printf("Área: %.2f Km²\n", area02);
-    printf("PIB: %.2f Bilhões de reais.\n", pib02);
+    printf("PIB: %.2f Bilhões de reais\n", pib02);
     printf("Número de Pontos Turísticos: %d\n", pontos02);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade02); //Atualização jogo super trunfo
+    printf("Pib Per capita: %.2f reais\n", per_capita02); //Atualização jogo super trunfo
 
 
 return 0;
